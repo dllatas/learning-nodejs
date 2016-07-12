cf84e0e055ebff7578f1cc1d768f0c76785911a6
dllatas <danllatas@gmail.com>
2016-07-12 11:32:49 +0200

    Folder restructuring

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # node
 Playing around with nodejs and express
+
+
+	installed
+
+	- mocha (unit testing)
+	- gulp (auto build)
+	- browserify (bundle.js)
+	- documentation JSDoc
+	- continuous integration (Travis CI)
+	- github pages (done)
+
+	
+	
+ 
--- basic/commands.js
+++ basic/commands.js
@@ -17,6 +17,26 @@ http.createServer(function (req, res) { res.writeHead(200, {"Content-Type":"text
 console.log("Server running at localhost:8124");
 http
 .hepl
+
+3 > 2 >1
+true == 1
+console.log("yo")
+console.log("yo");
+true === 1
+console.log("Hello World");
+myObj = {}
+myObj.list = ["a",1];
+myObj
+myObj.name = "Daniel";
+myObj
+myObj.getName = function(name) {}
+myObj.getName = function(name) {console.log(name);};
+myObj
+var http = require('http');
+http.createServer(function (req, res) { res.writeHead(200, {"Content-Type":"text/plain"}); res.end("Hello World\n");}).listen(8124, "127.0.0.1");
+console.log("Server running at localhost:8124");
+http
+.hepl
 var net = require("net");
 var chatServer = new.createServer();
 var chatServer = net.createServer();
